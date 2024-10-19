eacceaa51a60cbbe0cc03e722df9de29f2cc21f4
thanhdat1902 <thanhdat19022001@gmail.com>
2024-10-18 22:49:51 -0700

    fix: change logic to new connection_mongopy

--- db_connection_mongo.py
+++ db_connection_mongo.py
@@ -1,173 +1,111 @@
 #-------------------------------------------------------------------------
-# AUTHOR: Dat Nguyen
+# AUTHOR: DAT NGUYEN
 # FILENAME: db_connection_mongo.py
 # SPECIFICATION: PyMongo command to handle operation on MongoDB database
 # FOR: CS 5180- Assignment #2
 # TIME SPENT: 2 hours
 #-----------------------------------------------------------*/
 
+#IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with
+# standard arrays
+
+#importing some Python libraries
+# --> add your Python code here
 from pymongo import MongoClient
 import datetime
 import string
+from collections import defaultdict
 
 def connectDataBase():
-
-    # Creating a database connection object using pymongo
-
+    # Create a database connection object using pymongo
+    # --> add your Python code here
     DB_NAME = "CPP"
     DB_HOST = "localhost"
     DB_PORT = 27017
-
     try:
-
         client = MongoClient(host=DB_HOST, port=DB_PORT)
         db = client[DB_NAME]
-
         return db
 
     except:
         print("Database not connected successfully")
 
-def createDocument(documents, docId, docText, docTitle, docDate, docCat):
+def createDocument(col, docId, docText, docTitle, docDate, docCat):
 
+    # create a dictionary (document) to count how many times each term appears in the document.
+    # Use space " " as the delimiter character for terms and remember to lowercase them.
+    # --> add your Python code here
+    terms = docText.lower().translate(str.maketrans('', '', string.punctuation)).split(" ")
+    term_count = {}
+    for term in terms:
+        term_count[term] = term_count.get(term, 0) + 1
+
+    # create a list of dictionaries (documents) with each entry including a term, its occurrences, and its num_chars. Ex: [{term, count, num_char}]
+    # --> add your Python code here
+    termArray = []
+    totalNumchars = 0
+    for term, count in term_count.items():
+        termArray.append({"term": term, "count": count, "num_chars": len(term)})
+        totalNumchars+= len(term)*count
+
+
+    #Producing a final document as a dictionary including all the required fields
+    # --> add your Python code here
     docDate = datetime.datetime.strptime(docDate, "%Y-%m-%d")
-
-    # Value to be inserted
     document = {"_id": docId,
-            "text": docText,
-            "title": docTitle,
-            "date": docDate,
-            "category": docCat,
-            }
-
-    # Insert the document
-    documents.insert_one(document)
-
-def updateDocument(documents, docId, docText, docTitle, docDate, docCat):
-
-    docDate = datetime.datetime.strptime(docDate, "%Y-%m-%d")
-
-    # Document fields to be updated
-    document = {"$set":
-                    {
-                        "text": docText,
-                        "title": docTitle,
-                        "date": docDate,
-                        "category": docCat,
-                    }
+                "text": docText,
+                "title": docTitle,
+                "date": docDate,
+                "category": docCat,
+                "terms": termArray,
+                "num_chars": totalNumchars,
                 }
 
-    # Updating the document
-    documents.update_one({"_id": docId}, document)
+    # Insert the document
+    # --> add your Python code here
+    col.insert_one(document)
 
-def deleteDocument(documents, docId):
+
+def deleteDocument(col, docId):
+
     # Delete the document from the database
-    documents.delete_one({"_id": docId})
+    # --> add your Python code here
+    col.delete_one({"_id": docId})
 
+def updateDocument(col, docId, docText, docTitle, docDate, docCat):
 
-def getIndex(documents):
-    index = {}  # Initialize an empty dictionary to hold the inverted index
+    # Delete the document
+    # --> add your Python code here
+    col.delete_one({"_id": docId})
 
-    for document in documents.find():
-        docTitle = document["title"]
-        docText = document["text"]
+    # Create the document with the same id
+    # --> add your Python code here
+    createDocument(col, docId, docText, docTitle, docDate, docCat)
 
-        # Lowercase and remove punctuation
-        docText = docText.lower().translate(str.maketrans('', '', string.punctuation))
+def getIndex(col):
 
-        # Tokenize the document's text (split by whitespace)
-        terms = docText.split()
-
-        term_count = {}
-        for term in terms:
-            term_count[term] = term_count.get(term, 0) + 1
-
-        # Update the inverted index with terms and their counts in the current document
-        for term, count in term_count.items():
-            if term in index:
-                # Append the new reference to the existing term
-                index[term] += f", {docTitle}:{count}"
-            else:
-                # Create a new entry for the term
-                index[term] = f"{docTitle}:{count}"
-
-    return index
-
-
-
-# def createUser(col, id, name, email):
-#
-#     # Value to be inserted
-#     user = {"_id": id,
-#             "name": name,
-#             "email": email,
-#             }
-#
-#     # Insert the document
-#     col.insert_one(user)
-#
-# def updateUser(col, id, name, email):
-#
-#     # User fields to be updated
-#     user = {"$set": {"name": name, "email": email} }
-#
-#     # Updating the user
-#     col.update_one({"_id": id}, user)
-#
-# def deleteUser(col, id):
-#
-#     # Delete the document from the database
-#     col.delete_one({"_id": id})
-#
-# def getUser(col, id):
-#
-#     user = col.find_one({"_id":id})
-#
-#     if user:
-#         return str(user['_id']) + " | " + user['name'] + " | " + user['email']
-#     else:
-#         return []
-#
-# def createComment(col, id_user, dateTime, comment):
-#
-#     # Comments to be included
-#     comments = {"$push": {"comments": {
-#                                        "datetime": datetime.datetime.strptime(dateTime, "%m/%d/%Y %H:%M:%S"),
-#                                        "comment": comment
-#                                        } }}
-#
-#     # Updating the user document
-#     col.update_one({"_id": id_user}, comments)
-#
-# def updateComment(col, id_user, dateTime, new_comment):
-#
-#     # User fields to be updated
-#     comment = {"$set": {"comments.$.comment": new_comment} }
-#
-#     # Updating the user
-#     col.update_one({"_id": id_user, "comments.datetime": datetime.datetime.strptime(dateTime, "%m/%d/%Y %H:%M:%S")}, comment)
-#
-# def deleteComment(col, id_user, dateTime):
-#
-#     # Comments to be delete
-#     comments = {"$pull": {"comments": {"datetime": datetime.datetime.strptime(dateTime, "%m/%d/%Y %H:%M:%S")} }}
-#
-#     # Updating the user document
-#     col.update_one({"_id": id_user}, comments)
-#
-# def getChat(col):
-#
-#     # creating a document for each message
-#     pipeline = [
-#                  {"$unwind": { "path": "$comments" }},
-#                  {"$sort": {"comments.datetime": 1}}
-#                ]
-#
-#     comments = col.aggregate(pipeline)
-#
-#     chat = ""
-#
-#     for com in comments:
-#         chat += com['name'] + " | " + com['comments']['comment'] + " | " + str(com['comments']['datetime']) + "\n"
-#
-#     return chat
+    # Query the database to return the documents where each term occurs with their corresponding count. Output example:
+    # {'baseball':'Exercise:1','summer':'Exercise:1,California:1,Arizona:1','months':'Exercise:1,Discovery:3', ...}
+    # We are simulating an inverted index here in memory.
+    # --> add your Python code here
+    result = col.aggregate([{"$unwind": "$terms"},
+    {
+        "$project": {
+            "_id": 0,
+            "term": "$terms.term",
+            "documents": {
+                "$concat": [
+                    "$title",
+                    ":",
+                    {"$toString": "$terms.count"}
+                ]
+            }
+        }
+    },
+    {"$sort": {"term": 1}},
+    ])
+    termDict = defaultdict(list)
+    for doc in result:
+        termDict[doc['term']].append(doc['documents'])
+    termDict = {term: ','.join(entries) for term, entries in termDict.items()}
+    return termDict
